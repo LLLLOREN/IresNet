4e300b70e6f989622c41bf66e6296fd0772d68e2
LLLLoren <luoyongjia@mail.dlut.edu.cn>
2019-05-26 14:55:04 +0800

    update correlation func

--- models/IresNet.py
+++ models/IresNet.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+from .correlation_package.correlation import Correlation as corr
 import torch
 import torch.nn as nn
 import torch.utils.data
@@ -7,7 +8,7 @@ import torch.nn.functional as F
 import math
 import numpy as np
 
-def convbn(in_channel, out_channel, kernel_size, stride, pad, dilation):
+def conv(in_channel, out_channel, kernel_size, stride, pad, dilation = 1):
 
     return nn.Sequential(
         nn.Conv2d(
@@ -19,89 +20,77 @@ def convbn(in_channel, out_channel, kernel_size, stride, pad, dilation):
             dilation=dilation))
 
 
-def correlation(x, y, max_disp):
-    w = y.size()[2]
-    w = torch.Tensor()
-    corr_tensor = []
-    for i in range(-max_disp, 0, 1):
-        shifted = F.pad(y[:, :, 0:w + i, :],[[0, 0], [0, 0], [-i, 0],[0, 0]], mode = 'constant')
-        corr = torch.mean(shifted.mul(w),dim = 3)
-        corr_tensor.append(corr)
-    for i in range(max_disp + 1):
-        shifted = F.pad(x[:, :, i:, :],[[0, 0], [0, 0], [-i, 0], [0, 0]], mode = 'constant')
-        corr = torch.mean(shifted.mul(y), dim = 3)
-        corr_tensor.append(corr)
-    corr_tensor = torch.stack(corr_tensor)
-    return corr_tensor.permute(1, 2, 3, 0)
-
 class basisiResNet(nn.Module):
     def __init__(self):
         super(basisiResNet, self).__init__()
+
         #features extraction
-        self.conv1 = nn.Sequential(convbn(3, 64, 7, 2, 3, 1),
+        self.conv1 = nn.Sequential(conv(3, 64, 7, 2, 3),
                                    nn.LeakyReLU(negative_slope=0.1))
-        self.conv2 = nn.Sequential(convbn(64, 128, 5, 2, 2, 1),
+        self.conv2 = nn.Sequential(conv(64, 128, 5, 2, 2),
                                    nn.LeakyReLU(negative_slope=0.1))
         #for corr
-        self.corr_conv3 = nn.Sequential(convbn(128, 256, 3, 2, 1, 1),
+        self.corr_conv3 = nn.Sequential(conv(128, 256, 3, 2, 1),
                                         nn.LeakyReLU(negative_slope=0.1))
-        self.corr_conv3_1 = nn.Sequential(convbn(256, 256, 3, 1, 1, 1),
+        self.corr_conv3_1 = nn.Sequential(conv(256, 256, 3, 1, 1),
                                           nn.LeakyReLU(negative_slope=0.1))
         self.corr_deconv3 = nn.Sequential(nn.ConvTranspose2d(256, 128, 4, 2, 1))
-        self.corr_fusion2 = nn.Sequential(convbn(256, 128, 3, 1, 1, 1),
+        self.corr_fusion2 = nn.Sequential(conv(256, 128, 3, 1, 1),
                                           nn.LeakyReLU(negative_slope=0.1))
-        self.conv_redir = nn.Sequential(convbn(128, 64, 1, 1, 0, 1),
+        self.corr = nn.Sequential(corr(50, 1, 50, 1, 1))
+
+        self.conv_redir = nn.Sequential(conv(128, 64, 1, 1, 0),
                                         nn.LeakyReLU(negative_slope=0.1))
-        self.conv3 = nn.Sequential(convbn(145, 256, 5, 2, 2, 1),
+        self.conv3 = nn.Sequential(conv(145, 256, 5, 2, 2),
                                    nn.LeakyReLU(negative_slope=0.1))
-        self.conv3_1 = nn.Sequential(convbn(256, 256, 3, 1, 1, 1),
+        self.conv3_1 = nn.Sequential(conv(256, 256, 3, 1, 1),
                                      nn.LeakyReLU(negative_slope=0.1))
-        self.conv4 = nn.Sequential(convbn(256, 512, 3, 2,  1, 1),
+        self.conv4 = nn.Sequential(conv(256, 512, 3, 2,  1),
                                    nn.LeakyReLU(negative_slope=0.1))
-        self.conv4_1 = nn.Sequential(convbn(512, 512, 3, 1, 1, 1),
+        self.conv4_1 = nn.Sequential(conv(512, 512, 3, 1, 1),
                                      nn.LeakyReLU(negative_slope=0.1))
-        self.conv5 = nn.Sequential(convbn(512, 512, 3, 2, 1, 1),
+        self.conv5 = nn.Sequential(conv(512, 512, 3, 2, 1),
                                    nn.LeakyReLU(negative_slope=0.1))
-        self.conv5_1 = nn.Sequential(convbn(512, 512, 3, 1, 1, 1),
+        self.conv5_1 = nn.Sequential(conv(512, 512, 3, 1, 1),
                                      nn.LeakyReLU(negative_slope=0.1))
-        self.conv6 = nn.Sequential(convbn(512, 1024, 3, 2, 1, 1),
+        self.conv6 = nn.Sequential(conv(512, 1024, 3, 2, 1),
                                    nn.LeakyReLU(negative_slope=0.1))
-        self.conv6_1 = nn.Sequential(convbn(1024, 1024, 3, 1, 1, 1),
+        self.conv6_1 = nn.Sequential(conv(1024, 1024, 3, 1, 1),
                                      nn.LeakyReLU(negative_slope=0.1))
 
         #disparity regression
         self.upsample_flow = nn.Sequential(nn.ConvTranspose2d(1, 1, 4, 2, 1))
         #(6-1) regression
-        self.Convolution1 = nn.Sequential(convbn(1024, 1, 3, 1, 1, 1))
-        self.deconv5 = nn.Sequential(nn.ConvTranspose2d(1024, 512, 4, 2, 1),
+        self.Convolution1 = nn.Sequential(conv(1024, 1, 3, 1, 1))
+        self.deconv5 = nn.Sequential(nn.ConvTranspose2d(1024, 512, 4, 2),
                                      nn.LeakyReLU(negative_slope=0.1))
         #(5-1)
-        self.Convolution2 = nn.Sequential(convbn(1025, 512, 3, 1, 1, 1))
-        self.Convolution3 = nn.Sequential(convbn(512, 1, 3, 1, 1, 1))
+        self.Convolution2 = nn.Sequential(conv(1025, 512, 3, 1, 1))
+        self.Convolution3 = nn.Sequential(conv(512, 1, 3, 1, 1))
         #(5-2)
         self.deconv4 = nn.Sequential(nn.ConvTranspose2d(512, 256, 4, 2, 1),
                                      nn.LeakyReLU(negative_slope=0.1))
         #(4-1)
-        self.Convolution4 = nn.Sequential(convbn(769, 256, 3, 1, 1, 1))
-        self.Convolution5 = nn.Sequential(convbn(256, 1, 3, 1, 1, 1))
+        self.Convolution4 = nn.Sequential(conv(769, 256, 3, 1, 1))
+        self.Convolution5 = nn.Sequential(conv(256, 1, 3, 1, 1))
         #(4-2)
         self.deconv3 = nn.Sequential(nn.ConvTranspose2d(256, 128, 4, 2, 1),
                                      nn.LeakyReLU(negative_slope=0.1))
         #(3-1)
-        self.Convolution6 = nn.Sequential(convbn(385, 128, 3, 1, 1, 1))
-        self.Convolution7 = nn.Sequential(convbn(128, 1, 3, 1, 1, 1))
+        self.Convolution6 = nn.Sequential(conv(385, 128, 3, 1, 1))
+        self.Convolution7 = nn.Sequential(conv(128, 1, 3, 1, 1))
         #(3-2)
         self.deconv2 = nn.Sequential(nn.ConvTranspose2d(128, 64, 4, 2, 1),
                                      nn.LeakyReLU(negative_slope=0.1))
         #(2-1)
-        self.Convolution8 = nn.Sequential(convbn(193, 64, 3, 1, 1, 1))
-        self.Convolution9 = nn.Sequential(convbn(64, 1, 3, 1, 1, 1))
+        self.Convolution8 = nn.Sequential(conv(193, 64, 3, 1, 1))
+        self.Convolution9 = nn.Sequential(conv(64, 1, 3, 1, 1))
         #(2-2)
         self.deconv1 = nn.Sequential(nn.ConvTranspose2d(64, 32, 4, 2, 1),
                                      nn.LeakyReLU(negative_slope=0.1))
         #(1-1)
-        self.Convolution10 = nn.Sequential(convbn(97, 32, 3, 1, 1, 1))
-        self.Convolution11 = nn.Sequential(convbn(32, 1, 3, 1, 1, 1))
+        self.Convolution10 = nn.Sequential(conv(97, 32, 3, 1, 1))
+        self.Convolution11 = nn.Sequential(conv(32, 1, 3, 1, 1))
 
         #Multi-Scale-Full-Disparity
         # skip connections
@@ -109,7 +98,7 @@ class basisiResNet(nn.Module):
                                       nn.LeakyReLU(negative_slope=0.1))
         self.up_conv2ab = nn.Sequential(nn.ConvTranspose2d(128, 32, 8, 4, 2),
                                         nn.LeakyReLU(negative_slope=0.1))
-        self.up_conv12 = nn.Sequential(convbn(64, 32, 1, 1, 0, 1),
+        self.up_conv12 = nn.Sequential(conv(64, 32, 1, 1, 0),
                                        nn.LeakyReLU(negative_slope=0.1))
 
         # main branch
@@ -117,8 +106,8 @@ class basisiResNet(nn.Module):
                                      nn.LeakyReLU(negative_slope=0.1))
 
         # predict
-        self.Convolution21 = nn.Sequential(convbn(65, 32, 3, 1, 1, 1))
-        self.Convolution22 = nn.Sequential(convbn(32, 1, 3, 1, 1, 1))
+        self.Convolution21 = nn.Sequential(conv(65, 32, 3, 1, 1, 1))
+        self.Convolution22 = nn.Sequential(conv(32, 1, 3, 1, 1, 1))
 
         self.subupsample_felow6 = nn.Sequential(nn.ConvTranspose2d(1, 1, 128, 64, 32),
                                                 nn.LeakyReLU(negative_slope=0.1))
@@ -132,7 +121,7 @@ class basisiResNet(nn.Module):
                                                 nn.LeakyReLU(negative_slope=0.1))
         self.subupsample_felow1 = nn.Sequential(nn.ConvTranspose2d(1, 1, 4, 2, 1),
                                                 nn.LeakyReLU(negative_slope=0.1))
-        self.Convolution_predict_from_multi_res = nn.Sequential(convbn(7, 1, 1, 1, 0, 1),
+        self.Convolution_predict_from_multi_res = nn.Sequential(conv(7, 1, 1, 1, 0),
                                                                 nn.LeakyReLU(negative_slope=0, inplace=True))
 
     def forward(self, Limg, Rimg):
@@ -156,7 +145,7 @@ class basisiResNet(nn.Module):
         edcorr_2a = self.corr_fusion2(concat_edcorr_2a)
         edcorr_2b = self.corr_fusion2(concat_edcorr_2b)
 
-        corr = correlation(edcorr_2a, edcorr_2b, 40)
+        corr = self.corr(edcorr_2a, edcorr_2b)
         conv_redir = self.conv_redir(conv2a)
         output = torch.cat((corr, conv_redir),1)
         output = self.conv3(output)
@@ -249,8 +238,20 @@ class basisiResNet(nn.Module):
         return final_prediction,conv1a, conv1b, up_conv1b2b
 
 class refinementSub(nn.Module):
-    def __init__(self, conv1a, conv1b, final_prediction):
+    def __init__(self, conv1a, conv1b):
         super(refinementSub,self).__init__()
         self.conv1a = conv1a
         self.conv1b = conv1b
-        self.input = final_prediction
+        #iteration1 --sahre
+        #prepare input
+        self.compress_conv1a1b = nn.Sequential(conv(64, 16,3,1,1),
+                                               nn.LeakyReLU(negative_slope=0.1))
+        #iresnet
+        #itration 1 --changing
+        self.corr_min = nn.Sequential(corr(40, 1, 40, 1, 1))
+
+    def forward(self, x):
+        conv1a_mini = self.compress_conv1a1b(self.conv1a)
+        conv1b_mini = self.compress_conv1a1b(self.conv1b)
+        corr_mini = self.corr_mini(conv1a_mini, conv1b_mini)
+
